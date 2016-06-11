cf218abcccfdfc5d0ba86955221e022abfbd3b76
Clay Griswold <ccgriswold@gmail.com>
2016-06-11 16:41:04 -0400

    Another reattempt

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 /* jslint browser: true, esnext: true */
 
-// var angular = require('angular');
-// var angularRoute = require('angular-route');
-// require('./factory');
-// require('./controllers');
+var angular = require('angular');
+var angularRoute = require('angular-route');
+require('./factory');
+require('./controllers');
 
 var mainApp = angular.module('SiteApp', ['ngRoute', 'MySiteService', 'Controllers']);
 
@@ -25,29 +25,3 @@ mainApp.config(['$routeProvider', function($routeProvider){
       redirectTo: '/about',
     });
 }]);
-
-mainApp.exports = (function(){
-
-  var controllers = angular.module('Controllers', []);
-
-  controllers.controller('AboutViewController', ['$scope', function ($scope){
-    // console.log('main view');
-  }]);
-
-  controllers.controller('ProjectsViewController', ['$scope', function ($scope){
-    // console.log('projects view');
-  }]);
-
-  controllers.controller('ResumeViewController', ['$scope', function ($scope){
-    // console.log('resume view');
-  }]);
-});
-
-
-mainApp.exports = (function(){
-  var service = angular.module('MySiteService', []);
-
-  service.factory('MySiteService', function(){
-    // console.log('Hello');
-  });
-});
