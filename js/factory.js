07b5d48886840a07576205ab6030f18a05f486e7
Clay Griswold <ccgriswold@gmail.com>
2016-06-11 16:53:04 -0400

    New take on this

--- js/factory.js
+++
@@ -1,9 +0,0 @@
-/* jslint browser: true, esnext: true */
-
-module.exports = (function(){
-  var service = angular.module('MySiteService', []);
-
-  service.factory('MySiteService', function(){
-    // console.log('Hello');
-  });
-})();
